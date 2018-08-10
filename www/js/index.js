cf0fea9c4efceb72824f359ad47d456141dafa9b
wap4444 <wap4444@mail.ru>
2018-08-11 00:29:36 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -10,11 +10,9 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        alert('test');
 var ref = cordova.InAppBrowser.open('http://apache.org', '_blank', 'location=yes');
+        ref.show();
     }
 };
