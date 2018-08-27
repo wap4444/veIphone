9897b098ac386282cd6ca72d0969d98425328c72
wap4444 <wap4444@mail.ru>
2018-08-27 11:09:36 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,41 +1,13 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
-    // Update DOM on a Received Event
     receivedEvent: function(id) {
    document.addEventListener("offline", onOffline, false);
 function onOffline() {
@@ -46,11 +18,8 @@ document.addEventListener("online", onOnline, false);
 function onOnline() {
 var ref = cordova.InAppBrowser.open('http://elecor.controlsoft.kz/app/?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 }
-	    
 if(localStorage.ipush){}
-else{
-}
-		
+else{}
 
 $("#BtnEnt" ).click(function() {
 var ref = cordova.InAppBrowser.open('http://elecor.controlsoft.kz/app/?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
@@ -58,14 +27,13 @@ var ref = cordova.InAppBrowser.open('http://elecor.controlsoft.kz/app/?push='+lo
         
 function didReceiveRemoteNotificationCallBack(jsonData) {}
 function didOpenRemoteNotificationCallBack(jsonData) {
-	alert('PUSH'+jsonData.notification.payload.additionalData.ssylka);
+alert('PUSH'+jsonData.notification.payload.additionalData.ssylka);
 var ref = cordova.InAppBrowser.open(jsonData.notification.payload.additionalData.ssylka, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 }     
         //Настройка ПУШЕЙ ДЛЯ АЙФОНА
         var iosSettings = {};
         iosSettings["kOSSettingsKeyAutoPrompt"] = true;
         iosSettings["kOSSettingsKeyInAppLaunchURL"] = true;
-
         //ПОДКЛЮЧЕНИЕ ПУШЕЙ 
            window.plugins.OneSignal
           .startInit("52fd2532-f4de-48bd-a818-c918662346cd")
@@ -74,13 +42,10 @@ var ref = cordova.InAppBrowser.open(jsonData.notification.payload.additionalData
           .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.None)
           .iOSSettings(iosSettings)
           .endInit();
-        
 window.plugins.OneSignal.getIds(function(ids) {
 ipush = ids.userId;
 localStorage.ipush=ipush;
 var ref = cordova.InAppBrowser.open('http://elecor.controlsoft.kz/app/?push='+ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 });
-	    
-        
     }
 };
